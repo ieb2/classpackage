74817cf277463bd1613852ccc2dea8be9e4ff8f6
ieb2 <ieb2@students.uwf.edu>
2023-08-02 23:43:31 -0500

    Finalized normality and correlation function.

--- R/normality_and_correlation.R
+++ R/normality_and_correlation.R
@@ -1,15 +1,40 @@
+normal_correlation <- function(data, method){
 
-### DRAFT ###
-method <- "pearson"
+  numeric_subset <- data[sapply(data, is.numeric)]
 
-data <- drop_na(penguins)
-numeric_subset <- data[sapply(data, is.numeric)]
+  length_numeric_colnames <- length(colnames(numeric_subset))
 
-corr_mat <- cor(numeric_subset, method = method) %>% round(.,3)
+  qq_plots_list <- purrr::map(seq_len(length_numeric_colnames),
+                  ~one_qq_plot(numeric_subset, numeric_subset_names[[.x]]))
 
-normality_res <- lapply(numeric_subset, shapiro.test) %>%
-  do.call(rbind, .) %>%
-  data.frame() %>%
-  tibble::rownames_to_column("Variable") %>%
-  dplyr::select(-c(method, `data.name`)) %>%
-  dplyr::rename("Test Statistic" = "statistic", "p-value" = "p.value")
+  # In order to determine ideal qq plot layout
+  cols <- round(sqrt(length(qq_plots)),0)
+  rows <- ceiling(length(qq_plots)/cols)
+
+  qq_plots_out <-
+    ggpubr::ggarrange(plotlist = qq_plots_list, ncol = cols, nrow = rows)
+
+  corr_mat <- cor(numeric_subset, method = method)
+
+  normality_res <- lapply(numeric_subset, shapiro.test) %>%
+    do.call(rbind, .) %>%
+    data.frame() %>%
+    tibble::rownames_to_column("Variable") %>%
+    dplyr::select(-c(method, `data.name`)) %>%
+    dplyr::rename("Test Statistic" = "statistic", "p-value" = "p.value")
+
+out_list <- list("Normality Test" = normality_res,
+                 "Correlation Matrix" = corr_mat,
+                 "QQ Plots" = qq_plots_out)
+
+return(out_list)
+
+}
+
+
+
+
+
+
+
+
