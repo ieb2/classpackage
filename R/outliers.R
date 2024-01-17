867fea3a7ecf25de7229095a50b7e96f870da144
ieb2 <ieb2@students.uwf.edu>
2024-01-17 08:31:04 -0600

    New cooks function

--- R/outliers.R
+++ R/outliers.R
@@ -16,73 +16,96 @@
 #' @param threshold Determines the formula used in the calculation of the threshold value,
 #' used to determine whether an observation is an outlier. "baseR" (0.5 and 1), "matlab" (mean(cooksd)*3), and "convention" (4/n and 1).
 #' @param scale.factor Integer value that scales the point size and linewidth to allow customized viewing. Defaults to 0.5
+#' @param n_labels_desired Integer value that represents the number of labels to be displayed. If no value is supplied, all outliers are labeled.
+#' @param label_height_nudge Integer value that represents the label offset in relation to the upper end of the line. Defaults to 0.
 #' @return gg object
 #' @examples
 #' model <- lm(bill_length_mm ~ bill_depth_mm + year, penguins)
 #' cooks(model)
 #' @export
 cooks <-
-  function(fitted.lm, label = TRUE, show.threshold = FALSE, threshold = "convention", scale.factor = 0.5)
+  function(fitted.lm, label = TRUE, show.threshold = FALSE, threshold = "convention", scale.factor = 0.5,
+           n_labels_desired = NULL, label_height_nudge = 0)
   {
 
-  if ("lm" %in% class(fitted.lm) == FALSE) {
-    stop("Not a valid regression model.
+    if ("lm" %in% class(fitted.lm) == FALSE) {
+      stop("Not a valid regression model.
        Make sure object is created either via `glm`, lm` or `aov`")
-  }
+    }
 
-  # obtain linear model matrix
-  lm_matrix <- broom::augment(fitted.lm) %>%
-    as.data.frame()
-  lm_matrix[, "rowname"] <- 1:nrow(lm_matrix)
+    # obtain linear model matrix
+    lm_matrix <- broom::augment(fitted.lm) %>%
+      as.data.frame()
+    lm_matrix[, "rowname"] <- 1:nrow(lm_matrix)
 
-  # threshold for outlier
-  cooksd = lm_matrix[, ".cooksd"]
-  n = nrow(lm_matrix)
+    # threshold for outlier
+    cooksd = lm_matrix[, ".cooksd"]
+    n = nrow(lm_matrix)
 
-  # compute the threshold value for cook's distance plot
-  if (threshold == "matlab") {
-    threshold = mean(cooksd) * 3
-  }
-  else if (threshold == "baseR") {
-    threshold = c(0.5, 1)
-  }
-  else if (threshold == "convention") {
-    threshold = c(4/n, 1)
-  }
-  else {
-    stop("invalid threshold specified for gg_cooksd")
-  }
+    if(is.null(n_labels_desired)){
+      n_labels_desired <- n
+    } else{n_labels_desired <- n_labels_desired}
 
-  # window limit
-  limit = max(cooksd, na.rm = T)
-  margin_factor = 5
-  margin = round(limit / margin_factor)
-  max_cook = limit + margin
+    # compute the threshold value for cook's distance plot
+    if (threshold == "matlab") {
+      threshold = mean(cooksd) * 3
+    }
+    else if (threshold == "baseR") {
+      threshold = c(0.5, 1)
+    }
+    else if (threshold == "convention") {
+      threshold = c(4/n, 1)
+    }
+    else {
+      stop("invalid threshold specified for cooks")
+    }
 
-  .cooksd <- NULL
+    # window limit
+    limit = max(cooksd, na.rm = T)
+    margin_factor = 5
+    margin = round(limit / margin_factor)
+    max_cook = limit + margin
 
-  base_plot <- (ggplot2::ggplot(fitted.lm, aes(1:nrow(lm_matrix), .cooksd, ymin = 0, ymax = cooksd)) +
-                  ggplot2::geom_point(size = scale.factor) +
-                  ggplot2::geom_linerange(size = scale.factor) +
-                  ggplot2::xlab("Observation Number") +
-                  ggplot2::ylab("Cook's Distance") +
-                  ggplot2::ylim(0, max_cook+0.05)) +
-    ggplot2::theme_bw()
+    .cooksd <- NULL
 
-  # labelling of potential outliers
-  if (label) {
-    out_inds <- which(cooksd < min(threshold))
-    lm_matrix[out_inds, "rowname"] <- rep("", length(out_inds))
-    base_plot = base_plot +
-      ggplot2::geom_text(label = lm_matrix[, "rowname"], nudge_y = 0.05, color = "black")
-  }
+    base_plot <- (ggplot2::ggplot(fitted.lm, aes(1:nrow(lm_matrix), .cooksd, ymin = 0, ymax = cooksd)) +
+                    ggplot2::geom_point(size = scale.factor) +
+                    ggplot2::geom_linerange(linewidth = scale.factor) +
+                    ggplot2::xlab("Observation Number") +
+                    ggplot2::ylab("Cook's Distance") +
+                    ggplot2::ylim(0, max_cook+0.05)) +
+      ggplot2::theme_bw()
 
-  if (show.threshold) {
-    base_plot <- base_plot +
-      ggplot2::geom_hline(yintercept = threshold, linetype = "dashed")
-  }
+    # labelling of potential outliers
+    if (label) {
+      out_inds <- which(cooksd < min(threshold))
+      lm_matrix[out_inds, "rowname"] <- rep("", length(out_inds))
 
-  return(base_plot)
+      ordered_lm_matrix <-
+        lm_matrix[order(lm_matrix$.cooksd, decreasing = TRUE),]
+
+      for(i in 1:nrow(ordered_lm_matrix)){
+        if(i <= n_labels_desired){ordered_lm_matrix[i,]$rowname <-
+          ordered_lm_matrix[i,]$rowname}
+        else{ordered_lm_matrix[i,]$rowname <- ""}
+      }
+
+      lm_matrix <-
+        ordered_lm_matrix[order(as.numeric(row.names(ordered_lm_matrix))), ]
+
+      base_plot = base_plot +
+        ggplot2::geom_text(label = lm_matrix[, "rowname"], color = "black",
+                           aes(y = ifelse(cooksd+label_height_nudge < max_cook+0.05,
+                                          cooksd+label_height_nudge, cooksd)))
+
+    }
+
+    if (show.threshold) {
+      base_plot <- base_plot +
+        ggplot2::geom_hline(yintercept = threshold, linetype = "dashed")
+    }
+
+    return(base_plot)
   }
 
 
@@ -113,7 +136,7 @@ outlier_count <- function(df, model) {
     dplyr::summarise(count = n())
 }
 
-#' outlier_grpah
+#' outlier_graph
 #'
 #' Returns a scatterplot augmented with outlier information.
 #' @import ggplot2
